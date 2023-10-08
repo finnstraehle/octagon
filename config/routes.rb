7a2ab8ba909c54f2f38108550bacfa713f9697c7
Finn Straehle <finn.straehle1@gmx.ch>
2023-10-08 20:30:21 +0200

    styled home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/about', to: 'pages#about', as: :about
   get '/team', to: 'pages#team', as: :team
   get '/faq', to: 'pages#faq', as: :faq
